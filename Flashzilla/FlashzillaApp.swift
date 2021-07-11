54eef853a6091b1d2d2aaa263de40d33d1a42acd
Roman Kavinskyi <mrronanx@gmail.com>
2021-07-11 15:51:38 +0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Roman Kavinskyi on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
